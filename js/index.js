1a152ed4ac15b104901573c1dbeff3e12d9b47f9
ida O <ida.oussada@gmail.com>
2022-01-11 15:33:25 +0100

    1st part - get data from index.json if url fails

--- js/index.js
+++ js/index.js
@@ -5,13 +5,20 @@ window.onload = () => {
 
 async function render() {
     let response = await fetch('https://restcountries.com/v3.1/region/europe?fields=name');
-    let countries = await response.json();
+    if (!response.ok) {
+        response = await fetch('./json/index.json', {
+            headers: {
+            'Accept': 'application/json'
+            }
+        });
+    }
+    let countries = await response.json();    
     let list = '';
     // countries.sort((a,b) => a.name.official > b.name.official);
 
     countries.forEach(country => {
         list += `<li>${country.name.official}</li>`;
     });
-    
+
     document.querySelector('#countries-list').innerHTML = list;
 }
