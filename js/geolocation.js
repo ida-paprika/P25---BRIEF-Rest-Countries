f3baab56356fd5fcb843cdd618f640b47f827db7
ida O <ida.oussada@gmail.com>
2022-01-13 17:08:55 +0100

    4th Part - a little less messy

--- js/geolocation.js
+++ js/geolocation.js
@@ -3,49 +3,42 @@ window.onload = () => {
     render();
 }
 
-    /*
-    CLBRDLDSMTT :
-        - chargement initial extra-slow
-        - écoconception : appels d'API (rest country et/ou googleMaps)
-        - async des étapes pour éviter le flood dans la console
-        - c'est l'bordel ce code, va falloir refactorer è_é
-    */
-
 async function render() {
-    let response = await fetch('https://restcountries.com/v3.1/name/france?fields=name,capitalInfo,cca2');
-    const FRANCE = await response.json();
-    let select = document.querySelector('#selectCountry');
-    let options = `<option value="${FRANCE[0].cca2}">${FRANCE[0].name.official}</option>`;
-    let option;
 
-// reuse of "response" to fetch the full list of countries
-    response = await fetch('https://restcountries.com/v3.1/region/europe?fields=name,cca2');
+    let response = await fetch('https://restcountries.com/v3.1/region/europe?fields=name,cca2');
     const COUNTRIES = await response.json();
-    mapDisplay(FRANCE[0].capitalInfo.latlng[0], FRANCE[0].capitalInfo.latlng[1]);
+    let map = '';
+    let select = document.querySelector('#selectCountry');
+    let options = '';
+    let option;
     
-// get all countries except from France to create the other options
+// get all countries to create the select options
+// must use dispatchEvent instead of if-else to add 'selected'
     COUNTRIES.forEach(country => {
-        if (country.cca2 != FRANCE[0].cca2) {
+        if (country.cca2 == 'FR') {
+            options += `<option value="${country.cca2}" selected>${country.name.official}</option>`;
+        } else {
             options += `<option value="${country.cca2}">${country.name.official}</option>`;
         }
     });
+    select.innerHTML = options;
 
-    document.querySelector('#selectCountry').innerHTML = options;
-    
+// reuse of 'response' to fetch paris's geographic coordinates and display the map
+// doesn't feels right to fetch directly without using the countries list .json
+    response = await fetch(`https://restcountries.com/v3.1/alpha/fr?fields=capitalInfo`);
+    const FRANCE = await response.json();
+    map = `<iframe src="https://www.google.com/maps/embed/v1/view?key=AIzaSyCtI7aeTjJOVaB9Zc-r9m3mFHZnGcJoBZ4
+            &center=${FRANCE.capitalInfo.latlng[0]},${FRANCE.capitalInfo.latlng[1]}&zoom=10" 
+            allowfullscreen></iframe>`;
+    document.querySelector('#mapFrame').innerHTML = map;
+
 // change the map according to the selected country
     select.onchange = async () => {
         response = await fetch(`https://restcountries.com/v3.1/alpha/${select.value}?fields=capitalInfo`);
         option = await response.json();
-        mapDisplay(option.capitalInfo.latlng[0], option.capitalInfo.latlng[1]);
+        map = `<iframe src="https://www.google.com/maps/embed/v1/view?key=AIzaSyCtI7aeTjJOVaB9Zc-r9m3mFHZnGcJoBZ4
+                &center=${option.capitalInfo.latlng[0]},${option.capitalInfo.latlng[1]}&zoom=10" 
+                allowfullscreen></iframe>`;
+        document.querySelector('#mapFrame').innerHTML = map;
     }
-}
-
-
-// wildly taken from the internet
-function mapDisplay(lat, lng) {
-    var mapProp= {
-      center:new google.maps.LatLng(lat,lng),
-      zoom:10,
-    };
-    var map = new google.maps.Map(document.getElementById("mapFrame"),mapProp);
 }
